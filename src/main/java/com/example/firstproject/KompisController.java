51a42412c130ff84038fd90c419b05e518934b98
EmilWikell <91671600+EmilWikell@users.noreply.github.com>
2022-03-28 10:32:05 +0200

    postUpdate&Add

--- src/main/java/com/example/firstproject/KompisController.java
+++ src/main/java/com/example/firstproject/KompisController.java
@@ -1,10 +1,8 @@
 package com.example.firstproject;
 
 import org.apache.coyote.Response;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -13,10 +11,11 @@ import java.util.stream.Collectors;
 @RestController
 public class KompisController {
     Database dao =  new Database();
+    List<Kompis> kompisList = dao.getMyfriends();
 
     @RequestMapping("/kompisar")
     public List<Kompis> kompisar(){
-        return dao.getMyfriends();
+        return kompisList;
     }
 
     @RequestMapping("/kompis/{name}")
@@ -29,10 +28,47 @@ public class KompisController {
         for (Kompis kompis: dao.getMyfriends()) {
             if (kompis.getName().equals(name)){
                 dao.getMyfriends().remove(dao.getMyfriends().stream().filter(e -> e.getName().equals(kompis.getName())).toList().get(0)); // här borde vi kunna delete på namn bara
-
                 return new ResponseKompis("Friend deleted",true);
             }
         }
         return new ResponseKompis("Friend deleted", false);
+    }
+    @PostMapping("/kompis/addFriend")
+    public ResponseKompis addFriend(@RequestBody Kompis kompis){
+        ResponseKompis responseKompis = new ResponseKompis("Friend Added",Boolean.FALSE);
+        dao.getMyfriends().add(kompis);
+        responseKompis.setStatus(Boolean.TRUE);
+        return responseKompis;
+    }
+
+    @RequestMapping("/kompis/update")
+    public ResponseKompis upsertFriend(@RequestBody Kompis kompis) throws Exception {
+        ResponseKompis responseKompis = new ResponseKompis("Error", Boolean.FALSE);
+        Kompis existingFriend = findFriendById(kompis);
+        if (existingFriend !=null){
+            kompisList.set(getKompisListIndex(existingFriend),kompis);
+            responseKompis.setMessage("Friend Updated");
+            responseKompis.setStatus(Boolean.TRUE);
+        }else{
+            responseKompis.setMessage("Friend added");
+            responseKompis.setStatus(Boolean.TRUE);
+            dao.getMyfriends().add(kompis);
+        }
+        return responseKompis;
+    }
+
+    public Kompis findFriendById(Kompis kompis){
+         if (kompisList.stream().anyMatch(e-> e.getId() == kompis.getId())){
+             return kompisList.stream().filter(e-> e.getId() == kompis.getId()).toList().get(0);
+         }
+         else return null;
+    }
+    public int getKompisListIndex(Kompis existingFriend) throws Exception {
+        for (int i = 0; i < kompisList.size(); i++) {
+            if (existingFriend.getId() == kompisList.get(i).getId()) {
+                return i;
+            }
+        }
+        throw new Exception();
     }
 }
